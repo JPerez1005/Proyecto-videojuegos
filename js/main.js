a232c639f634ff1d48098e5ad9056b28c8d94c1b
name <correo>
2023-10-23 15:47:33 -0500

    cambios

--- js/main.js
+++ js/main.js
@@ -18,3 +18,18 @@ d.addEventListener("DOMContentLoaded",()=> {
     mVideojuego.addEventListener('click',modificarVideojuego);
     bbuscador.addEventListener('click',buscar);
 });
+
+const inputJuegos = d.getElementById("juegoBuscar");
+
+inputJuegos.addEventListener("keyup",() => {
+    let juegoBuscado = inputJuegos.value.toLowerCase();
+    listaPokemon.innerHTML = "";
+    for (let juego of juegos){
+        let nombrePoke = juego.name
+        if (nombrePoke.indexOf(juegoBuscado) !== -1){
+            mostrarPokemon(juego)
+        }else if(juegoBuscado==' '){
+            alert('madure')
+        }
+    }
+})
